7e0ac1ae2269189f35038698ea514814b4e4b7ed
alemba-21 <alembakiragu@gmail.com>
2023-02-22 09:04:18 +0300

    initializing the project

---
+++ src/Components/AllData.js
@@ -0,0 +1,28 @@
+import img1 from '../assets/img1.jpg';
+import img2 from '../assets/img2.jpg';
+import img3 from '../assets/img3.jpg';
+
+export default [
+    {
+        id: 1,
+        image: img1,
+        title: "UI/UX",
+        categories: "Designs",
+        description: "There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain...",
+    },
+    {
+        id: 2,
+        image: img2,
+        title: "E-Commerce",
+        categories: "Ecommerce",
+        description: "There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain...",
+    },
+    {
+        id: 3,
+        image: img3,
+        title: "API's",
+        categories: "Api",
+        description: "There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain...",
+    }
+
+]
